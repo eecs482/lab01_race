75aff322f39bf2053dcb05714217f27d69962682
Steven Xia <97299316+xia-steven@users.noreply.github.com>
2023-01-11 02:10:46 -0500

    Update race.cpp

--- race.cpp
+++ race.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <vector>
+
+using namespace std;
 
 const int NUM_THREADS = 2;
 const int NUM_ITER = 20;
 
 int counter = 0;
-std::mutex counter_mutex;
+mutex counter_mutex;
 
-void child()
-{
+void child() {
     for (int i=0; i < NUM_ITER; i++) {
-        counter_mutex.lock();
+        lock_guard<mutex> lock(counter_mutex); // RAII paradigm. std::lock_guard acquires the lock on construction and releases it on destruction
+        // counter_mutex.lock(); Taken care of by RAII
         counter++;
-        counter_mutex.unlock();
+        // counter_mutex.unlock(); Taken care of by RAII
     }
 }
 
-int main()
-{
-    std::thread* t[NUM_THREADS];
+int main() {
+    vector<thread> threads;
 
     for (int i=0; i < NUM_THREADS; i++) {
-        t[i] = new std::thread(child);
+        threads.emplace_back(child);
+        // This is an equivalent and simpler way to write
+        // threads.push_back(thread(child));
     }
 
-    for (int i=0; i < NUM_THREADS; i++) {
-        t[i]->join();
+    for (auto &t : threads) {
+        t.join();
     }
 
-    std::cout << "counter = " << counter << std::endl;
+    cout << "counter = " << counter << endl;
 }
 
